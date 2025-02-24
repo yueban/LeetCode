6f4d9574113fb706a0feec167b6634ac97dcd6ae
yueban <fbzhh007@gmail.com>
2025-02-24 17:12:25 +0800

    feat: implement product of array except self algorithms with tests

---
+++ src/main/kotlin/com/yueban/ProductExceptSelf.kt
@@ -0,0 +1,63 @@
+package com.yueban
+
+object ProductExceptSelf {
+  fun productExceptSelf1(nums: IntArray): IntArray {
+    val result = IntArray(nums.size) { 1 }
+    nums.forEachIndexed { index, num ->
+      for (resultIndex in result.indices) {
+        if (resultIndex != index) {
+          result[resultIndex] = result[resultIndex] * num
+        }
+      }
+    }
+    return result
+  }
+
+  fun productExceptSelf2(nums: IntArray): IntArray {
+    val forthProducts = IntArray(nums.size)
+    nums.forEachIndexed { index, num ->
+      if (index == 0) {
+        forthProducts[index] = 1 * num
+
+      } else {
+        forthProducts[index] = forthProducts[index - 1] * num
+      }
+    }
+
+    val backProducts = IntArray(nums.size)
+    for (index in nums.size - 1 downTo 0) {
+      if (index == nums.size - 1) {
+        backProducts[index] = 1 * nums[index]
+      } else {
+        backProducts[index] = backProducts[index + 1] * nums[index]
+      }
+    }
+
+    val result = IntArray(nums.size)
+    result.indices.forEach { index ->
+      result[index] = when (index) {
+        0 -> backProducts[1]
+        nums.size - 1 -> forthProducts[nums.size - 2]
+        else -> forthProducts[index - 1] * backProducts[index + 1]
+      }
+    }
+    return result
+  }
+
+  fun productExceptSelf3(nums: IntArray): IntArray {
+    var pre = 1
+    var post = 1
+
+    val result = IntArray(nums.size) { 1 }
+    for (i in 0 until nums.size) {
+      result[i] = pre
+      pre *= nums[i]
+    }
+
+    for (i in nums.size - 1 downTo 0) {
+      result[i] *= post
+      post *= nums[i]
+    }
+    return result
+  }
+}
---
+++ src/test/kotlin/com/yueban/ProductExceptSelfTest.kt
@@ -0,0 +1,40 @@
+package com.yueban
+
+import com.yueban.ProductExceptSelf.productExceptSelf1
+import com.yueban.ProductExceptSelf.productExceptSelf2
+import com.yueban.ProductExceptSelf.productExceptSelf3
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class ProductExceptSelfTest {
+  class TestCase(
+    val nums: IntArray,
+    val result: IntArray,
+  )
+
+  private val testCases = listOf(
+    TestCase(intArrayOf(1, 2, 3, 4), intArrayOf(24, 12, 8, 6)),
+    TestCase(intArrayOf(-1, 1, 0, -3, 3), intArrayOf(0, 0, 9, 0, 0))
+  )
+
+  @Test
+  fun containsDuplicate1Test() {
+    testCases.forEach { case ->
+      assertContentEquals(case.result, productExceptSelf1(case.nums))
+    }
+  }
+
+  @Test
+  fun containsDuplicate2Test() {
+    testCases.forEach { case ->
+      assertContentEquals(case.result, productExceptSelf2(case.nums))
+    }
+  }
+
+  @Test
+  fun containsDuplicate3Test() {
+    testCases.forEach { case ->
+      assertContentEquals(case.result, productExceptSelf3(case.nums))
+    }
+  }
+}
