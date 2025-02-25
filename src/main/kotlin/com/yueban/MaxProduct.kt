fb5453c5ff3dcbfa7716d879b3f095e1861d72aa
yueban <fbzhh007@gmail.com>
2025-02-25 15:03:05 +0800

    feat(algorithm): add max product subarray calculation with tests

---
+++ src/main/kotlin/com/yueban/MaxProduct.kt
@@ -0,0 +1,25 @@
+package com.yueban
+
+import kotlin.math.max
+import kotlin.math.min
+
+object MaxProduct {
+  fun maxProduct1(nums: IntArray): Int {
+    var res = Int.MIN_VALUE
+    for (n in nums) {
+      res = max(res, n)
+    }
+
+    var curMax = 1
+    var curMin = 1
+
+    for (n in nums) {
+      curMax = max(curMax * n, max((curMin * n), n))
+      curMin = min(curMax * n, min((curMin * n), n))
+
+      res = max(res, curMax)
+    }
+
+    return res
+  }
+}
---
+++ src/test/kotlin/com/yueban/MaxProductTest.kt
@@ -0,0 +1,25 @@
+package com.yueban
+
+import com.yueban.MaxProduct.maxProduct1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MaxProductTest {
+  class TestCase(
+    val nums: IntArray,
+    val result: Int,
+  )
+
+  private val testCases = listOf(
+    TestCase(intArrayOf(2, 3, -2, 4), 6),
+    TestCase(intArrayOf(-2, 0, -1), 0),
+    TestCase(intArrayOf(-3, -1, -1), 3),
+  )
+
+  @Test
+  fun maxProduct1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, maxProduct1(case.nums))
+    }
+  }
+}
