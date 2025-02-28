09055a43a01e7b154e1a0f9682fc7e6232bfb64c
yueban <fbzhh007@gmail.com>
2025-02-28 14:36:42 +0800

    feat(algorithm): implement search in rotated sorted array with tests

---
+++ src/main/kotlin/com/yueban/SearchInRotatedSortedArray.kt
@@ -0,0 +1,36 @@
+package com.yueban
+
+object SearchInRotatedSortedArray {
+  fun searchInRotatedSortedArray1(nums: IntArray, target: Int): Int {
+    for (i in 0 until nums.size) {
+      if (nums[i] == target) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  fun searchInRotatedSortedArray2(nums: IntArray, target: Int): Int {
+    var left = 0
+    var right = nums.size - 1
+    while (left <= right) {
+      val middle = (left + right) / 2
+      if (nums[middle] == target) return middle
+      if (nums[left] <= nums[middle]) {
+        if (nums[left] <= target && target < nums[middle]) {
+          right = middle - 1
+        } else {
+          left = middle + 1
+        }
+      } else {
+        if (nums[middle] < target && target <= nums[right]) {
+          left = middle + 1
+        } else {
+          right = middle - 1
+        }
+      }
+    }
+
+    return -1
+  }
+}
---
+++ src/test/kotlin/com/yueban/SearchInRotatedSortedArrayTest.kt
@@ -0,0 +1,34 @@
+package com.yueban
+
+import com.yueban.SearchInRotatedSortedArray.searchInRotatedSortedArray1
+import com.yueban.SearchInRotatedSortedArray.searchInRotatedSortedArray2
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SearchInRotatedSortedArrayTest {
+  class TestCase(
+    val nums: IntArray,
+    val target: Int,
+    val result: Int,
+  )
+
+  private val testCases = listOf(
+    TestCase(intArrayOf(4, 5, 6, 7, 0, 1, 2), 0, 4),
+    TestCase(intArrayOf(4, 5, 6, 7, 0, 1, 2), 3, -1),
+    TestCase(intArrayOf(1), 0, -1),
+  )
+
+  @Test
+  fun searchInRotatedSortedArray1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, searchInRotatedSortedArray1(case.nums, case.target))
+    }
+  }
+
+  @Test
+  fun searchInRotatedSortedArray2Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, searchInRotatedSortedArray2(case.nums, case.target))
+    }
+  }
+}
