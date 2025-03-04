c057771d8769bdb10391ec0af7dd07f4cd152c3d
yueban <fbzhh007@gmail.com>
2025-03-04 15:29:16 +0800

    feat(algorithm): add reverse bits implementation and corresponding tests

---
+++ src/main/kotlin/com/yueban/binary/ReserveBits.kt
@@ -0,0 +1,16 @@
+package com.yueban.binary
+
+object ReserveBits {
+  fun reverseBits1(n: Int): Int {
+    return n.toUInt().toString(2).reversed().padEnd(32, '0').toUInt(2).toInt()
+  }
+
+  fun reverseBits2(n: Int): Int {
+    tailrec fun reverseBitsTail(n: Int, bitsLength: Int, counter: Int, result: Int): Int = when (counter) {
+      bitsLength -> result
+      else -> reverseBitsTail(n ushr 1, bitsLength, counter + 1, (result shl 1) or (n and 1))
+    }
+
+    return reverseBitsTail(n, 32, 0, 0)
+  }
+}
---
+++ src/test/kotlin/com/yueban/binary/ReverseBitsTest.kt
@@ -0,0 +1,36 @@
+package com.yueban.binary
+
+import com.yueban.binary.ReserveBits.reverseBits1
+import com.yueban.binary.ReserveBits.reverseBits2
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ReverseBitsTest {
+  data class TestCase(
+    val n: Int,
+    val result: Int,
+  )
+
+  private val testCases = listOf(
+    TestCase(
+      "00000010100101000001111010011100".toUInt(2).toInt(), "00111001011110000010100101000000".toUInt(2).toInt()
+    ),
+    TestCase(
+      "11111111111111111111111111111101".toUInt(2).toInt(), "10111111111111111111111111111111".toUInt(2).toInt()
+    ),
+  )
+
+  @Test
+  fun reverseBits1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, reverseBits1(case.n))
+    }
+  }
+
+  @Test
+  fun reverseBits2Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, reverseBits2(case.n))
+    }
+  }
+}
