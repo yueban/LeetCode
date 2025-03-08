83ce738a9850d4cd187eb2589f1c583ffc4c7a6b
yueban <fbzhh007@gmail.com>
2025-03-08 18:23:12 +0800

    feat: add CombinationSumIV algorithm and corresponding tests

---
+++ src/main/kotlin/com/yueban/dynamic/CombinationSumIV.kt
@@ -0,0 +1,17 @@
+package com.yueban.dynamic
+
+object CombinationSumIV {
+  fun combinationSumIV1(nums: IntArray, target: Int): Int {
+    val dp = IntArray(target + 1)
+    dp[0] = 1
+
+    for (i in 1..target) {
+      for (n in nums) {
+        if (i - n >= 0) {
+          dp[i] += dp[i - n]
+        }
+      }
+    }
+    return dp[target]
+  }
+}
---
+++ src/test/kotlin/com/yueban/dynamic/CombinationSumIVTest.kt
@@ -0,0 +1,25 @@
+package com.yueban.dynamic
+
+import com.yueban.dynamic.CombinationSumIV.combinationSumIV1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CombinationSumIVTest {
+  class TestCase(
+    val nums: IntArray,
+    val target: Int,
+    val result: Int,
+  )
+
+  private val testCases = listOf(
+    TestCase(intArrayOf(1, 2, 3), 4, 7),
+    TestCase(intArrayOf(9), 3, 0),
+  )
+
+  @Test
+  fun combinationSumIV1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, combinationSumIV1(case.nums, case.target))
+    }
+  }
+}
