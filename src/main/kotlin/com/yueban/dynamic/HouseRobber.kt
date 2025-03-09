94e18be73a49764ba009ed8ec6b6a2423d8acc3d
yueban <fbzhh007@gmail.com>
2025-03-09 14:03:44 +0800

    feat: add HouseRobber algorithm with tests

---
+++ src/main/kotlin/com/yueban/dynamic/HouseRobber.kt
@@ -0,0 +1,20 @@
+package com.yueban.dynamic
+
+import kotlin.math.max
+
+object HouseRobber {
+  fun houseRobber1(nums: IntArray): Int {
+    if (nums.size == 1) return nums[0]
+    if (nums.size == 2) return max(nums[0], nums[1])
+
+    val dp = IntArray(nums.size)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for (i in 2 until nums.size) {
+      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+    }
+
+    return dp[nums.size - 1]
+  }
+}
---
+++ src/test/kotlin/com/yueban/dynamic/HouseRobberTest.kt
@@ -0,0 +1,24 @@
+package com.yueban.dynamic
+
+import com.yueban.dynamic.HouseRobber.houseRobber1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HouseRobberTest {
+  class TestCase(
+    val nums: IntArray,
+    val result: Int,
+  )
+
+  private val testCases = listOf(
+    TestCase(intArrayOf(1, 2, 3, 1), 4),
+    TestCase(intArrayOf(2, 7, 9, 3, 1), 12),
+  )
+
+  @Test
+  fun houseRobber1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, houseRobber1(case.nums))
+    }
+  }
+}
