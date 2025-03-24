147a59c9ecc9d3e711ce91758980f8c52cc020f3
yueban <fbzhh007@gmail.com>
2025-03-24 14:44:55 +0800

    feat: add algorithm implementation and tests for longest palindromic substring

---
+++ src/main/kotlin/com/yueban/string/LongestPalindromicSubstring.kt
@@ -0,0 +1,60 @@
+package com.yueban.string
+
+object LongestPalindromicSubstring {
+  fun longestPalindromicSubstring1(s: String): String {
+    var result = ""
+
+    for (i in 0 until s.length) {
+      // s[i] is the center of the palindromic substring
+      var step = 0
+      while (i - step >= 0 && i + step <= s.length - 1 && s[i - step] == s[i + step]) {
+        step++
+      }
+      step--
+      if (step * 2 + 1 > result.length) {
+        result = s.substring(i - step, i + step + 1)
+      }
+
+      // s[i],s[i+1] is the center of the palindromic substring
+      step = 0
+      while (i - step >= 0 && i + 1 + step <= s.length - 1 && s[i - step] == s[i + 1 + step]) {
+        step++
+      }
+      step--
+      if (step * 2 + 2 > result.length) {
+        result = s.substring(i - step, i + 2 + step)
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * basically same as [longestPalindromicSubstring1] but extract some common logic into findLongestPalindromicSubstring.
+   */
+  fun longestPalindromicSubstring2(s: String): String {
+    var result = ""
+
+    fun findLongestPalindromicSubstring(start: Int, end: Int) {
+      var l = start
+      var r = end
+      while (l >= 0 && r <= s.length - 1 && s[l] == s[r]) {
+        l--
+        r++
+      }
+
+      if (r - l - 1 > result.length) {
+        result = s.substring(l + 1, r)
+      }
+    }
+
+    for (i in 0 until s.length) {
+      // s[i] is the center of the palindromic substring
+      findLongestPalindromicSubstring(i, i)
+      // s[i],s[i+1] is the center of the palindromic substring
+      findLongestPalindromicSubstring(i, i + 1)
+    }
+
+    return result
+  }
+}
---
+++ src/test/kotlin/com/yueban/string/LongestPalindromicSubstringTest.kt
@@ -0,0 +1,33 @@
+package com.yueban.string
+
+import com.yueban.string.LongestPalindromicSubstring.longestPalindromicSubstring1
+import com.yueban.string.LongestPalindromicSubstring.longestPalindromicSubstring2
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class LongestPalindromicSubstringTest {
+  private class TestCase(
+    val s: String,
+    val result: String,
+  )
+
+  private val testCases = listOf(
+    TestCase("babad", "bab"),
+    TestCase("cbbd", "bb"),
+    TestCase("a", "a"),
+  )
+
+  @Test
+  fun longestPalindromicSubstring1Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, longestPalindromicSubstring1(case.s))
+    }
+  }
+
+  @Test
+  fun longestPalindromicSubstring2Test() {
+    testCases.forEach { case ->
+      assertEquals(case.result, longestPalindromicSubstring2(case.s))
+    }
+  }
+}
