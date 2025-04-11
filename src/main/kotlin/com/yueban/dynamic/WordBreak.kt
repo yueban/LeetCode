54243bbb370ac5004d90e56e6b82eaa683c20825
yueban <fbzhh007@gmail.com>
2025-04-11 11:34:33 +0800

    refactor: improve readability and comments in WordBreak.kt

--- src/main/kotlin/com/yueban/dynamic/WordBreak.kt
+++ src/main/kotlin/com/yueban/dynamic/WordBreak.kt
@@ -17,27 +17,30 @@ object WordBreak {
 
   fun wordBreak2(s: String, wordDict: List<String>): Boolean {
     fun dfs(s: String, dp: IntArray, start: Int, wordDict: MutableSet<String>): Boolean {
+      // start reaches s.length means the entire string has been successfully segmented
       if (start == s.length) {
         return true
       }
 
+      // if the substring from start has been computed before, return the stored result
       if (dp[start] != -1) {
         return dp[start] == 1
       }
 
       for (end in start + 1..s.length) {
         if (s.substring(start, end) in wordDict && dfs(s, dp, end, wordDict)) {
+          // mark the substring from start can be segmented
           dp[start] = 1
           return true
         }
       }
 
+      // mark the substring from start cannot be segmented
       dp[start] = 0
       return false
     }
 
-    val n = s.length
-    val dp = IntArray(n) { -1 }
+    val dp = IntArray(s.length) { -1 }
 
     return dfs(s, dp, 0, wordDict.toMutableSet())
   }
