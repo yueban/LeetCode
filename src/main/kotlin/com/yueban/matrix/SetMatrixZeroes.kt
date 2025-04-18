8bc24234522e64cba873cd375f48318342a0d7e2
yueban <fbzhh007@gmail.com>
2025-04-18 12:57:23 +0800

    chore: update comments in SetMatrixZeroes for clarity and accuracy

--- src/main/kotlin/com/yueban/matrix/SetMatrixZeroes.kt
+++ src/main/kotlin/com/yueban/matrix/SetMatrixZeroes.kt
@@ -1,10 +1,32 @@
 package com.yueban.matrix
 
+import com.yueban.matrix.SetMatrixZeroes.setMatrixZeroes1
+import com.yueban.matrix.SetMatrixZeroes.setMatrixZeroes2
+
 object SetMatrixZeroes {
-  /**
-   * use sets to filter same zero rows and columns to avoid redundant setting zeros in later steps.
-   */
   fun setMatrixZeroes1(matrix: Array<IntArray>) {
+    val zeros = mutableListOf<Pair<Int, Int>>()
+
+    val m = matrix.size
+    val n = matrix[0].size
+    for (i in 0 until m) {
+      for (j in 0 until n) {
+        if (matrix[i][j] == 0) {
+          zeros.add(i to j)
+        }
+      }
+    }
+
+    zeros.forEach { (i, j) ->
+      matrix[i].fill(0)
+      matrix.forEach { it[j] = 0 }
+    }
+  }
+
+  /**
+   * compare to [setMatrixZeroes1], filter same zero rows and columns by set to avoid redundant setting zeros in later steps.
+   */
+  fun setMatrixZeroes2(matrix: Array<IntArray>) {
     val rows = mutableSetOf<Int>()
     val columns = mutableSetOf<Int>()
 
@@ -29,34 +51,13 @@ object SetMatrixZeroes {
   }
 
   /**
-   * compare to [setMatrixZeroes1], there could be same row and column values in Pair, which may lead to redundant setting zeros in later steps.
-   */
-  fun setMatrixZeroes2(matrix: Array<IntArray>) {
-    val zeros = mutableListOf<Pair<Int, Int>>()
-
-    val m = matrix.size
-    val n = matrix[0].size
-    for (i in 0 until m) {
-      for (j in 0 until n) {
-        if (matrix[i][j] == 0) {
-          zeros.add(i to j)
-        }
-      }
-    }
-
-    zeros.forEach { (i, j) ->
-      matrix[i].fill(0)
-      matrix.forEach { it[j] = 0 }
-    }
-  }
-
-  /**
-   * compare to [setMatrixZeroes1], this method have a better space complexity which is O(1).
+   * compare to [setMatrixZeroes2], this method have a better space complexity which is O(1).
    * it uses matrix itself to temporary mark zero values, so it doesn't need additional space to store zeros on the first iteration.
    */
   fun setMatrixZeroes3(matrix: Array<IntArray>) {
-    // use row (index 1) to mark zeros for all columns
-    // use column (index 0) to mark zeros for all rows
+    // use first row to mark all columns which need to be set to zeros
+    // use first column to mark all rows which need to be set to zeros (except first row)
+    // use firstRow to mark if first row itself needs to be set to zeros
     var firstRow = 1
     for (i in 0 until matrix.size) {
       for (j in 0 until matrix[i].size) {
@@ -71,8 +72,8 @@ object SetMatrixZeroes {
       }
     }
 
-    // iterate from column 1 to end, if there is zero in the first row for current column, set the entire column to zero
-    // except column 0 because it is used to mark zeros for all rows
+    // check first row, set columns to zeros if needed
+    // except first column which is used to mark zeros for rows
     for (j in 1 until matrix[0].size) {
       if (matrix[0][j] == 0) {
         for (i in 0 until matrix.size) {
@@ -81,7 +82,7 @@ object SetMatrixZeroes {
       }
     }
 
-    // iterate from row 1 to end, if there is zero in the first column for current row, set the entire row to zero
+    // check first column, set rows to zeros if needed except first row, cause first row is
     // except row 0 because it is used to mark zeros for all columns
     for (i in 1 until matrix.size) {
       if (matrix[i][0] == 0) {
@@ -91,15 +92,15 @@ object SetMatrixZeroes {
       }
     }
 
-    // now all the columns and rows are set to zero if needed (except row 0 and column 0)
-    // set column 0 to zeros if needed
+    // now all the columns and rows are set to zero if needed (except first row and first column)
+    // first column now can be set to zeros if needed
     if (matrix[0][0] == 0) {
       for (i in 1 until matrix.size) {
         matrix[i][0] = 0
       }
     }
 
-    // set row 0 to zeros if needed
+    // first row can be set to zeros if needed
     if (firstRow == 0) {
       for (j in 0 until matrix[0].size) {
         matrix[0][j] = 0
