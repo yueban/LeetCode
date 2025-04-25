e7f9592898339c2f8f5199e25d257005188b2341
yueban <fbzhh007@gmail.com>
2025-04-25 13:01:24 +0800

    refactor(tree): rename helper functions for clarity in BinaryTreeMaximumPathSum

--- src/main/kotlin/com/yueban/tree/BinaryTreeMaximumPathSum.kt
+++ src/main/kotlin/com/yueban/tree/BinaryTreeMaximumPathSum.kt
@@ -1,5 +1,6 @@
 package com.yueban.tree
 
+import com.yueban.tree.BinaryTreeMaximumPathSum.binaryTreeMaximumPathSum1
 import kotlin.math.max
 
 object BinaryTreeMaximumPathSum {
@@ -10,23 +11,24 @@ object BinaryTreeMaximumPathSum {
    * 3. maxPathSum(node right child) + node
    * 4. maxPathSum(node left child) + node + maxPathSum(node right child)
    *
-   * But the 4th scenario is not a valid result when iterating (function maxSinglePathSum), because the 4th scenario
-   * has already used both left and right child nodes, so the 4th scenario cannot be connected by the parent again.
+   * But the 4th scenario is not a valid maxOneSide when iterating (function maxSinglePathSum), because the 4th
+   * scenario has already used both left and right child nodes, so the 4th scenario cannot be connected by the parent again.
+   *
    * But the 4th scenario is valid when calculating max result.
    */
   fun binaryTreeMaximumPathSum1(root: TreeNode?): Int {
     var result = Int.MIN_VALUE
 
-    fun maxSinglePathSum(node: TreeNode?): Int {
+    fun maxOneSideSum(node: TreeNode?): Int {
       if (node == null) return 0
-      val maxLeft = maxSinglePathSum(node.left)
-      val maxRight = maxSinglePathSum(node.right)
-      val maxOfSinglePath = maxOf(node.`val`, maxLeft + node.`val`, maxRight + node.`val`)
-      result = maxOf(maxOfSinglePath, maxLeft + maxRight + node.`val`, result)
-      return maxOfSinglePath
+      val maxLeft = maxOneSideSum(node.left)
+      val maxRight = maxOneSideSum(node.right)
+      val maxOneSide = maxOf(node.`val`, maxLeft + node.`val`, maxRight + node.`val`)
+      result = maxOf(maxOneSide, maxLeft + maxRight + node.`val`, result)
+      return maxOneSide
     }
 
-    maxSinglePathSum(root)
+    maxOneSideSum(root)
 
     return result
   }
@@ -38,18 +40,18 @@ object BinaryTreeMaximumPathSum {
   fun binaryTreeMaximumPathSum2(root: TreeNode?): Int {
     var result = Int.MIN_VALUE
 
-    fun dfs(node: TreeNode?): Int {
+    fun maxOneSideSum(node: TreeNode?): Int {
       if (node == null) return 0
 
-      val leftMax = max(0, dfs(node.left))
-      val rightMax = max(0, dfs(node.right))
+      val leftMax = max(0, maxOneSideSum(node.left))
+      val rightMax = max(0, maxOneSideSum(node.right))
 
       result = max(result, node.`val` + leftMax + rightMax)
 
       return max(leftMax, rightMax) + node.`val`
     }
 
-    dfs(root)
+    maxOneSideSum(root)
 
     return result
   }
