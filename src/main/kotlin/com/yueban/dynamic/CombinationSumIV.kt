9eb3c12f11f85168629745b24e949d0eb93c3b0a
yueban <fbzhh007@gmail.com>
2025-05-12 14:19:16 +0800

    docs: add comment explaining dp array purpose in CombinationSumIV

--- src/main/kotlin/com/yueban/dynamic/CombinationSumIV.kt
+++ src/main/kotlin/com/yueban/dynamic/CombinationSumIV.kt
@@ -1,6 +1,9 @@
 package com.yueban.dynamic
 
 object CombinationSumIV {
+  /**
+   * `dp[i]` represents the number of possible combinations that add up to `i`.
+   */
   fun combinationSumIV1(nums: IntArray, target: Int): Int {
     val dp = IntArray(target + 1)
     dp[0] = 1
