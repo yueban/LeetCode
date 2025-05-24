d6487a2ffdeb02632267b670b7d2878a432ab697
yueban <fbzhh007@gmail.com>
2025-05-24 16:15:05 +0800

    refactor: simplify and optimize longestSubstringWithoutRepeatingCharacters functions and update tests

--- src/main/kotlin/com/yueban/string/LongestSubstringWithoutRepeatingCharacters.kt
+++ src/main/kotlin/com/yueban/string/LongestSubstringWithoutRepeatingCharacters.kt
@@ -1,36 +1,25 @@
 package com.yueban.string
 
+import com.yueban.string.LongestSubstringWithoutRepeatingCharacters.longestSubstringWithoutRepeatingCharacters1
 import kotlin.math.max
 
 object LongestSubstringWithoutRepeatingCharacters {
+  /**
+   * use a slide window to track distinct chars. when iterating, check the position of `s[i]` appeared last time,
+   * move `l` to make sure current window doesn't contain two `s[i]`, keep going to make sure current window doesn't
+   * have repeating chars.
+   */
   fun longestSubstringWithoutRepeatingCharacters1(s: String): Int {
-    val set = mutableSetOf<Char>()
-    var result = 0
-    var l = 0
+    if (s.length <= 1) return s.length
 
-    for (i in 0 until s.length) {
-      if (set.contains(s[i])) {
-        result = max(result, i - l)
-        set.clear()
-        l = i
-      }
-      set.add(s[i])
-    }
-
-    return result
-  }
-
-  fun longestSubstringWithoutRepeatingCharacters2(s: String): Int {
     val map = mutableMapOf<Char, Int>()
     var result = 0
     var l = 0
 
     for (i in 0 until s.length) {
-      if (map.containsKey(s[i]) && map[s[i]]!! >= l) {
-        l = map[s[i]]!! + 1
-      } else {
-        result = max(result, i - l + 1)
-      }
+      val char = s[i]
+      l = max(l, map.getOrDefault(char, -1) + 1)
+      result = max(result, i - l + 1)
       map[s[i]] = i
     }
 
@@ -38,11 +27,9 @@ object LongestSubstringWithoutRepeatingCharacters {
   }
 
   /**
-   * use a slide window to track distinct chars. when iterating, check the position of `s[i]` appeared last time,
-   * move `l` to make sure current window doesn't contain two `s[i]`, keep going to make sure current window doesn't
-   * have repeating chars.
+   * basically same as [longestSubstringWithoutRepeatingCharacters1], but use array for a better performance.
    */
-  fun longestSubstringWithoutRepeatingCharacters3(s: String): Int {
+  fun longestSubstringWithoutRepeatingCharacters2(s: String): Int {
     if (s.length <= 1) return s.length
 
     // min value of Char.code is 32 which is space, max value of Char.code is 126 which is '~'
--- src/test/kotlin/com/yueban/string/LongestSubstringWithoutRepeatingCharactersTest.kt
+++ src/test/kotlin/com/yueban/string/LongestSubstringWithoutRepeatingCharactersTest.kt
@@ -2,7 +2,6 @@ package com.yueban.string
 
 import com.yueban.string.LongestSubstringWithoutRepeatingCharacters.longestSubstringWithoutRepeatingCharacters1
 import com.yueban.string.LongestSubstringWithoutRepeatingCharacters.longestSubstringWithoutRepeatingCharacters2
-import com.yueban.string.LongestSubstringWithoutRepeatingCharacters.longestSubstringWithoutRepeatingCharacters3
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -16,6 +15,9 @@ class LongestSubstringWithoutRepeatingCharactersTest {
     TestCase("abcabcbb", 3),
     TestCase("bbbbb", 1),
     TestCase("pwwkew", 3),
+    TestCase(" ", 1),
+    TestCase("abcdaefg", 7),
+    TestCase("au", 2),
   )
 
   @Test
@@ -29,13 +31,6 @@ class LongestSubstringWithoutRepeatingCharactersTest {
   fun longestSubstringWithoutRepeatingCharacters2Test() {
     testCases.forEach { case ->
       assertEquals(case.result, longestSubstringWithoutRepeatingCharacters2(case.s))
-    }
-  }
-
-  @Test
-  fun longestSubstringWithoutRepeatingCharacters3Test() {
-    testCases.forEach { case ->
-      assertEquals(case.result, longestSubstringWithoutRepeatingCharacters3(case.s))
     }
   }
 }
